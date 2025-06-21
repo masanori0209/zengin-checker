0996e71068d34e3a6d9ff0683e25e286cc6b1ce6
masa0209 <masanorimurakoshi0209@gmail.com>
2025-06-21 17:42:39 +0900

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/zengin-checker/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
     sourcemap: false,
   },
